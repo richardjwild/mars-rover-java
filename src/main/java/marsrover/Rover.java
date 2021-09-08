d75884d92398e338dcd8a06ff9bd66d75c1dc51e
Richard Wild <richard.wild@codurance.com>
2021-09-08 17:33:05 +0100

    Rename direction field to heading

--- src/main/java/marsrover/Rover.java
+++ src/main/java/marsrover/Rover.java
@@ -2,26 +2,26 @@ package marsrover;
 
 public class Rover {
 
-  private final Direction direction;
+  private final Direction heading;
   private final Vector position;
   private final Grid world;
 
-  public Rover(Direction direction, Vector position, Grid world) {
-    this.direction = direction;
+  public Rover(Direction heading, Vector position, Grid world) {
+    this.heading = heading;
     this.position = position;
     this.world = world;
   }
 
   public String getState() {
-    return String.format("%s:%s", position.toString(), direction.getAbbreviation());
+    return String.format("%s:%s", position.toString(), heading.getAbbreviation());
   }
 
   public Rover perform(Command command) {
     switch (command) {
       case LEFT:
-        return turn(direction.getLeft());
+        return turn(heading.getLeft());
       case RIGHT:
-        return turn(direction.getRight());
+        return turn(heading.getRight());
       default:
         return move();
     }
@@ -32,12 +32,12 @@ public class Rover {
   }
 
   private Rover move() {
-    var translation = direction.getTranslation();
+    var translation = heading.getTranslation();
     var newPosition = position.apply(translation);
     if (world.isOccupied(newPosition))
-      return new BlockedRover(direction, position, world);
+      return new BlockedRover(heading, position, world);
     else
-      return new Rover(direction, world.wrap(newPosition), world);
+      return new Rover(heading, world.wrap(newPosition), world);
   }
 
 }
