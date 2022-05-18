5372d3f78c6a0b23cfede70f0d80eed65e47c262
Richard Wild <richard.wild@codurance.com>
2022-05-18 10:10:42 +0100

    Convert to JUnit 5

--- src/test/java/marsrover/AcceptanceTest.java
+++ src/test/java/marsrover/AcceptanceTest.java
@@ -1,30 +1,30 @@
 package marsrover;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Ignore;
-import org.junit.Test;
-
-public class AcceptanceTest {
+class AcceptanceTest {
 
   @Test
-  public void mars_rover_drives_around() {
+  void mars_rover_drives_around() {
     var marsRoverDriver = new MarsRoverDriver();
     var rover = marsRoverDriver.drive("MMRMMLM");
     assertThat(rover.getState()).isEqualTo("2:3:N");
   }
 
   @Test
-  @Ignore
-  public void mars_rover_wraps_around_at_edge_of_grid() {
+  @Disabled
+  void mars_rover_wraps_around_at_edge_of_grid() {
     var marsRoverDriver = new MarsRoverDriver();
     var rover = marsRoverDriver.drive("MMMMMMMMMM");
     assertThat(rover.getState()).isEqualTo("0:0:N");
   }
 
   @Test
-  @Ignore
-  public void mars_rover_stops_when_meeting_an_obstacle() {
+  @Disabled
+  void mars_rover_stops_when_meeting_an_obstacle() {
     var marsRoverDriver = new MarsRoverDriver(new Obstacle(0, 3));
     var rover = marsRoverDriver.drive("MMMM");
     assertThat(rover.getState()).isEqualTo("O:0:2:N");
