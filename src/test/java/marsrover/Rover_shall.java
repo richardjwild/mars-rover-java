503ea28a494e7ba76b754d25d35bcf0ef0b9386c
Richard Wild <richard.wild@codurance.com>
2022-06-05 14:36:45 +0100

    Remove assertj from PBT

--- src/test/java/marsrover/Rover_shall.java
+++ src/test/java/marsrover/Rover_shall.java
@@ -5,7 +5,6 @@ import net.jqwik.api.*;
 import static java.lang.String.format;
 import static marsrover.Command.*;
 import static marsrover.Direction.*;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @Group
 class Rover_shall {
@@ -21,13 +20,13 @@ class Rover_shall {
         class when_turning_right {
 
             @Property
-            void face_east_with_position_unchanged(
+            boolean face_east_with_position_unchanged(
                     @ForAll("x positions") int x,
                     @ForAll("y positions") int y)
             {
                 var rover = new Rover(NORTH, new Vector(x, y), WORLD);
                 var expectedState = format("%d:%d:%s", x, y, EAST.getAbbreviation());
-                assertThat(rover.perform(RIGHT).getState()).isEqualTo(expectedState);
+                return rover.perform(RIGHT).getState().equals(expectedState);
             }
         }
 
@@ -35,13 +34,13 @@ class Rover_shall {
         class when_turning_left {
 
             @Property
-            void face_west_with_position_unchanged(
+            boolean face_west_with_position_unchanged(
                     @ForAll("x positions") int x,
                     @ForAll("y positions") int y)
             {
                 var rover = new Rover(NORTH, new Vector(x, y), WORLD);
                 var expectedState = format("%d:%d:%s", x, y, WEST.getAbbreviation());
-                assertThat(rover.perform(LEFT).getState()).isEqualTo(expectedState);
+                return rover.perform(LEFT).getState().equals(expectedState);
             }
         }
 
@@ -49,20 +48,20 @@ class Rover_shall {
         class when_moving_forward {
 
             @Property
-            void increment_its_y_position_with_direction_unchanged(
+            boolean increment_its_y_position_with_direction_unchanged(
                     @ForAll("x positions") int x,
                     @ForAll("y positions except north edge") int y)
             {
                 var rover = new Rover(NORTH, new Vector(x, y), WORLD);
                 var expectedState = format("%d:%d:%s", x, y + 1, NORTH.getAbbreviation());
-                assertThat(rover.perform(MOVE).getState()).isEqualTo(expectedState);
+                return rover.perform(MOVE).getState().equals(expectedState);
             }
 
             @Property
-            void wrap_over_edge_of_grid(@ForAll("x positions") int x) {
+            boolean wrap_over_northern_edge_of_grid(@ForAll("x positions") int x) {
                 var rover = new Rover(NORTH, new Vector(x, HEIGHT - 1), WORLD);
                 var expectedState = format("%d:%d:%s", x, 0, NORTH.getAbbreviation());
-                assertThat(rover.perform(MOVE).getState()).isEqualTo(expectedState);
+                return rover.perform(MOVE).getState().equals(expectedState);
             }
         }
     }
@@ -74,13 +73,13 @@ class Rover_shall {
         class when_turning_right {
 
             @Property
-            void face_south_with_position_unchanged(
+            boolean face_south_with_position_unchanged(
                     @ForAll("x positions") int x,
                     @ForAll("y positions") int y)
             {
                 var rover = new Rover(EAST, new Vector(x, y), WORLD);
                 var expectedState = format("%d:%d:%s", x, y, SOUTH.getAbbreviation());
-                assertThat(rover.perform(RIGHT).getState()).isEqualTo(expectedState);
+                return rover.perform(RIGHT).getState().equals(expectedState);
             }
         }
 
@@ -88,13 +87,13 @@ class Rover_shall {
         class when_turning_left {
 
             @Property
-            void face_north_with_position_unchanged(
+            boolean face_north_with_position_unchanged(
                     @ForAll("x positions") int x,
                     @ForAll("y positions") int y)
             {
                 var rover = new Rover(EAST, new Vector(x, y), WORLD);
                 var expectedState = format("%d:%d:%s", x, y, NORTH.getAbbreviation());
-                assertThat(rover.perform(LEFT).getState()).isEqualTo(expectedState);
+                return rover.perform(LEFT).getState().equals(expectedState);
             }
         }
 
@@ -102,20 +101,20 @@ class Rover_shall {
         class when_moving_forward {
 
             @Property
-            void increment_its_x_position_with_direction_unchanged(
+            boolean increment_its_x_position_with_direction_unchanged(
                     @ForAll("x positions except east edge") int x,
                     @ForAll("y positions") int y)
             {
                 var rover = new Rover(EAST, new Vector(x, y), WORLD);
                 var expectedState = format("%d:%d:%s", x + 1, y, EAST.getAbbreviation());
-                assertThat(rover.perform(MOVE).getState()).isEqualTo(expectedState);
+                return rover.perform(MOVE).getState().equals(expectedState);
             }
 
             @Property
-            void wrap_over_edge_of_grid(@ForAll("y positions") int y) {
+            boolean wrap_over_eastern_edge_of_grid(@ForAll("y positions") int y) {
                 var rover = new Rover(EAST, new Vector(WIDTH - 1, y), WORLD);
                 var expectedState = format("%d:%d:%s", 0, y, EAST.getAbbreviation());
-                assertThat(rover.perform(MOVE).getState()).isEqualTo(expectedState);
+                return rover.perform(MOVE).getState().equals(expectedState);
             }
         }
     }
@@ -127,13 +126,13 @@ class Rover_shall {
         class when_turning_right {
 
             @Property
-            void face_west_with_position_unchanged(
+            boolean face_west_with_position_unchanged(
                     @ForAll("x positions") int x,
                     @ForAll("y positions") int y)
             {
                 var rover = new Rover(SOUTH, new Vector(x, y), WORLD);
                 var expectedState = format("%d:%d:%s", x, y, WEST.getAbbreviation());
-                assertThat(rover.perform(RIGHT).getState()).isEqualTo(expectedState);
+                return rover.perform(RIGHT).getState().equals(expectedState);
             }
         }
 
@@ -141,13 +140,13 @@ class Rover_shall {
         class when_turning_left {
 
             @Property
-            void face_east_with_position_unchanged(
+            boolean face_east_with_position_unchanged(
                     @ForAll("x positions") int x,
                     @ForAll("y positions") int y)
             {
                 var rover = new Rover(SOUTH, new Vector(x, y), WORLD);
                 var expectedState = format("%d:%d:%s", x, y, EAST.getAbbreviation());
-                assertThat(rover.perform(LEFT).getState()).isEqualTo(expectedState);
+                return rover.perform(LEFT).getState().equals(expectedState);
             }
         }
 
@@ -155,20 +154,20 @@ class Rover_shall {
         class when_moving_forward {
 
             @Property
-            void decrement_its_y_position_with_direction_unchanged(
+            boolean decrement_its_y_position_with_direction_unchanged(
                     @ForAll("x positions") int x,
                     @ForAll("y positions except south edge") int y)
             {
                 var rover = new Rover(SOUTH, new Vector(x, y), WORLD);
                 var expectedState = format("%d:%d:%s", x, y - 1, SOUTH.getAbbreviation());
-                assertThat(rover.perform(MOVE).getState()).isEqualTo(expectedState);
+                return rover.perform(MOVE).getState().equals(expectedState);
             }
 
             @Property
-            void wrap_over_edge_of_grid(@ForAll("x positions") int x) {
+            boolean wrap_over_southern_edge_of_grid(@ForAll("x positions") int x) {
                 var rover = new Rover(SOUTH, new Vector(x, 0), WORLD);
                 var expectedState = format("%d:%d:%s", x, HEIGHT - 1, SOUTH.getAbbreviation());
-                assertThat(rover.perform(MOVE).getState()).isEqualTo(expectedState);
+                return rover.perform(MOVE).getState().equals(expectedState);
             }
         }
     }
@@ -180,13 +179,13 @@ class Rover_shall {
         class when_turning_right {
 
             @Property
-            void face_north_with_position_unchanged(
+            boolean face_north_with_position_unchanged(
                     @ForAll("x positions") int x,
                     @ForAll("y positions") int y)
             {
                 var rover = new Rover(WEST, new Vector(x, y), WORLD);
                 var expectedState = format("%d:%d:%s", x, y, NORTH.getAbbreviation());
-                assertThat(rover.perform(RIGHT).getState()).isEqualTo(expectedState);
+                return rover.perform(RIGHT).getState().equals(expectedState);
             }
         }
 
@@ -194,13 +193,13 @@ class Rover_shall {
         class when_turning_left {
 
             @Property
-            void face_south_with_position_unchanged(
+            boolean face_south_with_position_unchanged(
                     @ForAll("x positions") int x,
                     @ForAll("y positions") int y)
             {
                 var rover = new Rover(WEST, new Vector(x, y), WORLD);
                 var expectedState = format("%d:%d:%s", x, y, SOUTH.getAbbreviation());
-                assertThat(rover.perform(LEFT).getState()).isEqualTo(expectedState);
+                return rover.perform(LEFT).getState().equals(expectedState);
             }
         }
 
@@ -208,20 +207,20 @@ class Rover_shall {
         class when_moving_forward {
 
             @Property
-            void decrement_its_x_position_with_direction_unchanged(
+            boolean decrement_its_x_position_with_direction_unchanged(
                     @ForAll("x positions except west edge") int x,
                     @ForAll("y positions") int y)
             {
                 var rover = new Rover(WEST, new Vector(x, y), WORLD);
                 var expectedState = format("%d:%d:%s", x - 1, y, WEST.getAbbreviation());
-                assertThat(rover.perform(MOVE).getState()).isEqualTo(expectedState);
+                return rover.perform(MOVE).getState().equals(expectedState);
             }
 
             @Property
-            void wrap_over_edge_of_grid(@ForAll("y positions") int y) {
+            boolean wrap_over_western_edge_of_grid(@ForAll("y positions") int y) {
                 var rover = new Rover(WEST, new Vector(0, y), WORLD);
                 var expectedState = format("%d:%d:%s", WIDTH - 1, y, WEST.getAbbreviation());
-                assertThat(rover.perform(MOVE).getState()).isEqualTo(expectedState);
+                return rover.perform(MOVE).getState().equals(expectedState);
             }
         }
     }
